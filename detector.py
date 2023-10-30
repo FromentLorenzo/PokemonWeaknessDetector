c86f0501e871072afe26181774d26338c19660cf
Lorenzo Froment <lorenzo.froment@etu.unice.fr>
2023-10-30 20:20:10 +0100

    print weakness on cmd with manual execution

--- detector.py
+++ detector.py
@@ -1,37 +1,37 @@
 import pyautogui
 from PIL import Image
 import pytesseract
+import requests
 
-# Spécifiez les coordonnées de la région de l'écran à capturer (x, y, largeur, hauteur)
-region = (300, 40, 380, 100)
+# Spécifiez les coordonnées de la région de l'écran à capturer (x, y, hauteur, largeur)
+region = (300, 70, 280, 68)
 screenshot = pyautogui.screenshot(region=region)
 
 # Convertissez la capture d'écran en une image Pillow
 image = Image.frombytes("RGB", screenshot.size, screenshot.tobytes())
-
-# Affichez l'image
-
 
 # Utilisez pytesseract pour extraire du texte de l'image
 text = pytesseract.image_to_string(image)
+morceaux = text.split(' ', 1)
+text = morceaux[0]
+# Affichez le texte extrait
+print("Texte extrait de l'image:", text)
 
-# Vérifiez si le mot que vous cherchez est présent dans le texte
-if "votre_mot" in text:
-    print("Le mot a été détecté à l'écran.")
+# Formatez l'URL avec le nom du Pokémon extrait
+url = f"https://pokebuildapi.fr/api/v1/pokemon/{text}"
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    # Extrait les informations du Pokémon
+    name = data["name"]
+    print(f"Nom : {name}")
+    filtered_resistances = [resistance for resistance in data["apiResistances"] if resistance["damage_multiplier"] in {2, 4}]
+
+# Affichez les résistances avec damage_multiplier égal à 2 ou 4
+    for resistance in filtered_resistances:
+        print(resistance)
+
+    
 else:
-    print("Le mot n'a pas été détecté à l'écran.")
-
-print(text)
-
-
-# Spécifiez les coordonnées de la région de l'écran à capturer (x, y, largeur, hauteur)
-region = (100, 100, 800, 600)
-screenshot = pyautogui.screenshot(region=region)
-
-# Convertissez la capture d'écran en une image Pillow
-image = Image.frombytes("RGB", screenshot.size, screenshot.tobytes())
-
-# Affichez l'image
-image.show()
-
-
+    print("Aucun Pokémon ne correspond à votre recherche.")
